777e9cf3348d5765436e439c8ea9ec707062a7c9
Jeremy Tien <jeremy29tien@gmail.com>
2023-01-30 17:58:29 -0800

    debug env issue

---
+++ robosuite/environments/manipulation/lift_gym.py
@@ -0,0 +1,17 @@
+from robosuite.wrappers import GymWrapper
+import robosuite as suite
+
+
+class LiftJaco(GymWrapper):
+    def __init__(self, keys=None):
+        env = suite.make(
+            "Lift",
+            robots="Jaco",  # use Jaco robot -- TODO: can we leave this argument out?
+            use_camera_obs=False,  # do not use pixel observations
+            has_offscreen_renderer=False,  # not needed since not using pixel obs
+            has_renderer=False,  # make sure we can render to the screen
+            reward_shaping=True,  # use dense rewards -- TODO: change this?
+            control_freq=20,  # control should happen fast enough so that simulation looks smooth
+        )
+        # Run super method
+        super().__init__(env=env, keys=keys)
