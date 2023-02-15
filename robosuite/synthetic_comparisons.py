8c7125aa090431e80f6ad0166b150eba06d52c54
Jeremy Tien <jeremy29tien@gmail.com>
2023-02-15 15:43:34 -0800

    add features

--- robosuite/synthetic_comparisons.py
+++ robosuite/synthetic_comparisons.py
@@ -1,5 +1,5 @@
 import numpy as np
-from robosuite.environments.manipulation.lift_features import speed, height, distance_to_bottle
+from robosuite.environments.manipulation.lift_features import speed, height, distance_to_bottle, distance_to_cube
 
 
 greater_speed_adjs = ["faster", "quicker", "swifter", "at a higher speed"]
@@ -46,11 +46,25 @@ def generate_synthetic_comparisons(traj1, traj2, feature_name):
         traj2_feature_values = [distance_to_bottle(traj2[t]) for t in range(horizon)]
 
         if np.min(traj1_feature_values) > np.min(traj2_feature_values):  # Here, we take the MINIMUM distance
-            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in greater_distance_adjs]
-            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in less_distance_adjs]
+            ordinary_comps = ["The first trajectory is " + w + " from the bottle than the second trajectory." for w in greater_distance_adjs]
+            flipped_comps = ["The second trajectory is " + w + " to the botte than the first trajectory." for w in less_distance_adjs]
             return ordinary_comps + flipped_comps
         else:
-            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in less_distance_adjs]
-            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in greater_distance_adjs]
+            # The first trajectory is further from the bottle than the second trajectory.
+            ordinary_comps = ["The first trajectory is " + w + " to the bottle than the second trajectory." for w in less_distance_adjs]
+            flipped_comps = ["The second trajectory is " + w + " from the bottle than the first trajectory." for w in greater_distance_adjs]
+            return ordinary_comps + flipped_comps
+
+    elif feature_name == "distance_to_cube":
+        traj1_feature_values = [distance_to_cube(traj1[t]) for t in range(horizon)]
+        traj2_feature_values = [distance_to_cube(traj2[t]) for t in range(horizon)]
+
+        if np.min(traj1_feature_values) > np.min(traj2_feature_values):  # Here, we take the MINIMUM distance
+            ordinary_comps = ["The first trajectory is " + w + " from the cube than the second trajectory." for w in greater_distance_adjs]
+            flipped_comps = ["The second trajectory is " + w + " to the cube than the first trajectory." for w in less_distance_adjs]
+            return ordinary_comps + flipped_comps
+        else:
+            ordinary_comps = ["The first trajectory is " + w + " to the cube than the second trajectory." for w in less_distance_adjs]
+            flipped_comps = ["The second trajectory is " + w + " from the cube than the first trajectory." for w in greater_distance_adjs]
             return ordinary_comps + flipped_comps
 
