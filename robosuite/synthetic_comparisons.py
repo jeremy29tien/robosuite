ba3ed1956e0b58f84ca2a131963e4ba788b3ee6a
Jeremy Tien <jeremy29tien@gmail.com>
2023-02-15 12:36:03 -0800

    clean up code and add more comparison words

--- robosuite/synthetic_comparisons.py
+++ robosuite/synthetic_comparisons.py
@@ -2,6 +2,14 @@ import numpy as np
 from robosuite.environments.manipulation.lift_features import speed, height, distance_to_bottle
 
 
+greater_speed_adjs = ["faster", "quicker", "swifter", "at a higher speed"]
+less_speed_adjs = ["slower", "more moderate", "more sluggish", "at a lower speed"]
+greater_height_adjs = ["higher", "taller", "at a greater height"]
+less_height_adjs = ["lower", "shorter", "at a lesser height"]
+greater_distance_adjs = ["further", "farther", "more distant"]
+less_distance_adjs = ["closer", "nearer", "more nearby"]
+
+
 # This function takes in two trajectories in the form of LISTS of (observation, action) pairs.
 def generate_synthetic_comparisons(traj1, traj2, feature_name):
     horizon = len(traj1)
@@ -12,37 +20,37 @@ def generate_synthetic_comparisons(traj1, traj2, feature_name):
         traj2_feature_values = [speed(traj2[t]) for t in range(horizon)]
 
         if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):
-            return ["The first trajectory is faster than the second trajectory.",
-                    "The second trajectory is slower than the first trajectory."
-                    ]
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in greater_speed_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in less_speed_adjs]
+            return ordinary_comps + flipped_comps
         else:
-            return ["The first trajectory is slower than the second trajectory.",
-                    "The second trajectory is faster than the first trajectory."
-                    ]
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in less_speed_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in greater_speed_adjs]
+            return ordinary_comps + flipped_comps
 
     elif feature_name == "height":
         traj1_feature_values = [height(traj1[t]) for t in range(horizon)]
         traj2_feature_values = [height(traj2[t]) for t in range(horizon)]
 
         if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):
-            return ["The first trajectory is higher than the second trajectory.",
-                    "The second trajectory is lower than the first trajectory."
-                    ]
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in greater_height_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in less_height_adjs]
+            return ordinary_comps + flipped_comps
         else:
-            return ["The first trajectory is lower than the second trajectory.",
-                    "The second trajectory is higher than the first trajectory."
-                    ]
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in less_height_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in greater_height_adjs]
+            return ordinary_comps + flipped_comps
 
     elif feature_name == "distance_to_bottle":
         traj1_feature_values = [distance_to_bottle(traj1[t]) for t in range(horizon)]
         traj2_feature_values = [distance_to_bottle(traj2[t]) for t in range(horizon)]
 
         if np.mean(traj1_feature_values) > np.mean(traj2_feature_values):
-            return ["The first trajectory is further from the bottle than the second trajectory.",
-                    "The second trajectory is closer to the bottle than the first trajectory."
-                    ]
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in greater_distance_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in less_distance_adjs]
+            return ordinary_comps + flipped_comps
         else:
-            return ["The first trajectory is closer to the bottle than the second trajectory.",
-                    "The second trajectory is further from the bottle than the first trajectory."
-                    ]
+            ordinary_comps = ["The first trajectory is " + w + " than the second trajectory." for w in less_distance_adjs]
+            flipped_comps = ["The second trajectory is " + w + " than the first trajectory." for w in greater_distance_adjs]
+            return ordinary_comps + flipped_comps
 
