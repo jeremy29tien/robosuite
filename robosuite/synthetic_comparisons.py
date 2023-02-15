792f9f1a225ddf82ddb665f335f683fd258b7287
Jeremy Tien <jeremy29tien@gmail.com>
2023-02-15 15:47:26 -0800

    change the distance to cube feature function to compare based on the FINAL distance to cube

--- robosuite/synthetic_comparisons.py
+++ robosuite/synthetic_comparisons.py
@@ -59,7 +59,7 @@ def generate_synthetic_comparisons(traj1, traj2, feature_name):
         traj1_feature_values = [distance_to_cube(traj1[t]) for t in range(horizon)]
         traj2_feature_values = [distance_to_cube(traj2[t]) for t in range(horizon)]
 
-        if np.min(traj1_feature_values) > np.min(traj2_feature_values):  # Here, we take the MINIMUM distance
+        if traj1_feature_values[-1] > traj2_feature_values[-1]:  # Here, we take the FINAL distance
             ordinary_comps = ["The first trajectory is " + w + " from the cube than the second trajectory." for w in greater_distance_adjs]
             flipped_comps = ["The second trajectory is " + w + " to the cube than the first trajectory." for w in less_distance_adjs]
             return ordinary_comps + flipped_comps
